ea0c14acc5849525c1b2bcc227a74ef07be600ea
matosistvan <istvan.matos@seon.io>
2022-04-02 17:03:52 +0200

    Initial commit

---
+++ src/test/java/com/example/contractgenerator/ContractGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contractgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
